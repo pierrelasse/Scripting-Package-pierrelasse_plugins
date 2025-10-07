7951888bb0b04492b27a4787cc186b38868faef2
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2025-10-07 13:04:03 +0200

    pierrelasse/plugins/switchAccount: add persistent option, fixes

--- @pierrelasse/plugins/switchAccount.lua
+++ @pierrelasse/plugins/switchAccount.lua
@@ -7,11 +7,14 @@ local profileResolver = require("@pierrelasse/lib/profileResolver")
 
 local this = {
     COMMAND = "switchaccount",
-    PERMISSION = "switchaccount"
+    PERMISSION = "switchaccount",
+    ALLOW_PERSISTENT = true
 }
 
 ---@type java.Map<string, java.Object>
 this.map = java.map()
+---@type java.Set<string>
+this.persistent = java.set()
 
 events.onStarted(function()
     commands.add(this.COMMAND, function(sender, args)
@@ -31,12 +34,37 @@ events.onStarted(function()
             return
         end
 
-        local target = bukkit.player(args[2] or sender.getName())
-        if target == nil then
-            bukkit.send(sender, "§cPlayer not found!")
+        local persistent = false
+
+        local target
+        if args[2] == nil then
+            target = sender
+        else
+            if args[2] == "-p" then
+                target = sender
+                persistent = true
+            else
+                target = bukkit.offlinePlayer(args[2])
+                if target == nil then
+                    bukkit.send(sender, "§cPlayer not found!")
+                    return
+                end
+            end
+        end
+
+        if args[3] == "-p" then
+            persistent = true
+        end
+
+        if this.persistent.remove(target.getName()) then
+            bukkit.send(sender, "§2"..target.getName().."§a is no longer marked as persistent!")
             return
         end
 
+        if not target.isOnline() then
+            bukkit.send(sender, "§cPlayer not found!")
+            return
+        end
 
         local function set(profile)
             local playerProfile = CraftPlayerProfile(
@@ -45,26 +73,29 @@ events.onStarted(function()
             )
 
             this.map.put(target.getName(), playerProfile)
+            if persistent and this.ALLOW_PERSISTENT then
+                this.persistent.add(target.getName())
+            end
 
             bukkit.send(sender, "§2"..target.getName().."§a will now log in as §2"..profile.name.."§a the next time!")
         end
 
-        local offlinePlayer = bukkit.offlinePlayer(name)
-        if offlinePlayer ~= nil then
-            set({ uuid = offlinePlayer.getUniqueId(), name = offlinePlayer.getName() })
-            return
-        end
-
-        bukkit.send(sender, "§7Resolving...")
         tasks.async(function()
-            local result, err = profileResolver.fetch(name)
-            tasks.wait(0, function()
-                if result == nil then
-                    bukkit.send(sender, "§cCould not fetch UUID! §7("..err..")")
+            local result
+
+            local offlinePlayer = bukkit.offlinePlayer(name)
+            if offlinePlayer ~= nil then
+                result = { uuid = offlinePlayer.getUniqueId(), name = offlinePlayer.getName() }
+            else
+                local _result, err = profileResolver.fetch(name)
+                if _result == nil then
+                    bukkit.send(sender, "§cCould not fetch profile! §7("..err..")")
                     return
                 end
-                set(result)
-            end)
+                result = _result
+            end
+
+            tasks.wait(0, function() set(result) end)
         end)
     end)
         .permission(this.PERMISSION)
@@ -76,23 +107,30 @@ events.onStarted(function()
                         return i.getName()
                     end)
             elseif #args == 2 then
-                complete(completions, args[1],
+                complete(completions, args[2],
                     bukkit.playersLoop(), function(i)
                         ---@cast i bukkit.entity.Player
                         return i.getName()
                     end)
+            elseif #args == 3 then
+                complete(completions, args[2], { "-p" })
             end
         end)
-
-    events.listen(AsyncPlayerPreLoginEvent, function(event)
-        local name = event.getName() ---@type string
-
-        local changeTo = this.map.remove(name)
-        if changeTo == nil then return end
-
-        event.setPlayerProfile(changeTo)
-    end)
-        .priority("LOWEST")
 end)
+
+-- Make this listener execute as early as possible.
+events.listen(AsyncPlayerPreLoginEvent, function(event)
+    local name = event.getName() ---@type string
+
+    local changeTo = this.map.get(name)
+    if changeTo == nil then return end
+
+    if not this.persistent.contains(name) then
+        this.map.remove(name)
+    end
+
+    event.setPlayerProfile(changeTo)
+end)
+    .priority("LOWEST")
 
 return this
