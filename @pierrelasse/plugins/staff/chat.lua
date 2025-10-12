7a6392afbe0c5d85a4b0d12d5e7d439789867a22
pierrelasse <76223390+pierrelasse@users.noreply.github.com>
2025-10-12 17:13:38 +0200

    pierrelasse/plugins/staff/chat: use logger

--- @pierrelasse/plugins/staff/chat.lua
+++ @pierrelasse/plugins/staff/chat.lua
@@ -2,34 +2,29 @@ local nameFormatter = require("@pierrelasse/lib/nameFormatter")
 
 
 local this = {
-    PREFIX = comp.from("§3[§bS Chat§3] "),
-
     COMMAND = { "staffchat", "sc" },
     PERMISSION = "!.staff.chat"
 }
 
-events.onStarted(function()
-    commands.add(this.COMMAND, function(sender, args)
-        ---@cast sender bukkit.entity.Player
+this.log = require("@pierrelasse/plugins/staff/log"):sub("chat", "Chat", function(player)
+    return player.hasPermission(this.PERMISSION)
+end)
 
+events.onStarted(function()
+    commands.add(this.COMMAND, function(sender, args) ---@cast sender bukkit.entity.Player
         local message = table.concat(args, " ")
         if #message == 0 then
             bukkit.send(sender, "§cUsage: /staffchat <message...>")
             return
         end
 
-        Lang.sendMult(
-            function(l)
-                return comp.empty()
-                    .append(this.PREFIX)
-                    .append(comp.from(nameFormatter.prefixName(sender)))
-                    .append(comp.text(":").color(comp.colorN("GRAY")))
-                    .appendSpace()
-                    .append(comp.text(message).color(comp.colorHex("#e6fffe")))
-            end,
-            bukkit.playersLoop(), function(p)
-                return p.hasPermission(this.PERMISSION)
-            end)
+        this.log:log(function(l)
+            return comp.empty()
+                .append(comp.from(nameFormatter.prefixName(sender)))
+                .append(comp.text(":").color(comp.colorN("GRAY")))
+                .appendSpace()
+                .append(comp.text(message).color(comp.colorHex("#e6fffe")))
+        end)
     end)
         .permission(this.PERMISSION)
         .complete(function(completions, sender, args)
